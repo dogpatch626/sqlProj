8a66143dba2a8ac1af3cd44029c2ea04de3a18e3
Bin Huang <44174709+ibhuang@users.noreply.github.com>
2020-12-01 18:13:32 -0500

    Update DML.sql
    aa

--- DML.sql
+++ DML.sql
@@ -1,52 +1,132 @@
-insert into States values
-(NULL, 'Alabama', 'AL'),
-(NULL, 'Alaska', 'AK'),
-(NULL, 'Arizona', 'AZ'),
-(NULL, 'Arkansas', 'AR'),
-(NULL, 'California', 'CA'),
-(NULL, 'Colorado', 'CO'),
-(NULL, 'Connecticut', 'CT'),
-(NULL, 'Delaware', 'DE'),
-(NULL, 'District of Columbia', 'DC'),
-(NULL, 'Florida', 'FL'),
-(NULL, 'Georgia', 'GA'),
-(NULL, 'Hawaii', 'HI'),
-(NULL, 'Idaho', 'ID'),
-(NULL, 'Illinois', 'IL'),
-(NULL, 'Indiana', 'IN'),
-(NULL, 'Iowa', 'IA'),
-(NULL, 'Kansas', 'KS'),
-(NULL, 'Kentucky', 'KY'),
-(NULL, 'Louisiana', 'LA'),
-(NULL, 'Maine', 'ME'),
-(NULL, 'Maryland', 'MD'),
-(NULL, 'Massachusetts', 'MA'),
-(NULL, 'Michigan', 'MI'),
-(NULL, 'Minnesota', 'MN'),
-(NULL, 'Mississippi', 'MS'),
-(NULL, 'Missouri', 'MO'),
-(NULL, 'Montana', 'MT'),
-(NULL, 'Nebraska', 'NE'),
-(NULL, 'Nevada', 'NV'),
-(NULL, 'New Hampshire', 'NH'),
-(NULL, 'New Jersey', 'NJ'),
-(NULL, 'New Mexico', 'NM'),
-(NULL, 'New York', 'NY'),
-(NULL, 'North Carolina', 'NC'),
-(NULL, 'North Dakota', 'ND'),
-(NULL, 'Ohio', 'OH'),
-(NULL, 'Oklahoma', 'OK'),
-(NULL, 'Oregon', 'OR'),
-(NULL, 'Pennsylvania', 'PA'),
-(NULL, 'Rhode Island', 'RI'),
-(NULL, 'South Carolina', 'SC'),
-(NULL, 'South Dakota', 'SD'),
-(NULL, 'Tennessee', 'TN'),
-(NULL, 'Texas', 'TX'),
-(NULL, 'Utah', 'UT'),
-(NULL, 'Vermont', 'VT'),
-(NULL, 'Virginia', 'VA'),
-(NULL, 'Washington', 'WA'),
-(NULL, 'West Virginia', 'WV'),
-(NULL, 'Wisconsin', 'WI'),
-(NULL, 'Wyoming', 'WY');
+delete from address;
+delete from application;
+delete from certifications_financials;
+delete from company_information;
+delete from company_sector;
+delete from company_subsector;
+delete from country;
+delete from customer; 
+delete from cyber_security_clearence;
+delete from cyber_security_questions;
+delete from foreign_associates;
+delete from funding;
+delete from imported_materials;
+delete from investors;
+delete from material; 
+delete from naics_codes;
+delete from negative_regulatory_actions;
+delete from patents;
+delete from revenue;
+delete from sectors;
+delete from states;
+delete from subsectors;
+
+insert into States (Abbreviation,Name) values ('AL','Alabama');
+insert into States (Abbreviation,Name) values ('AK','Alaska');
+insert into States (Abbreviation,Name) values ('AS','American Samoa');
+insert into States (Abbreviation,Name) values ('AZ','Arizona');
+insert into States (Abbreviation,Name) values ('AR','Arkansas');
+insert into States (Abbreviation,Name) values ('CA','California');
+insert into States (Abbreviation,Name) values ('CO','Colorado');
+insert into States (Abbreviation,Name) values ('CT','Connecticut');
+insert into States (Abbreviation,Name) values ('DE','Delaware');
+insert into States (Abbreviation,Name) values ('DC','District of Columbia');
+insert into States (Abbreviation,Name) values ('FM','Federated States of Micronesia');
+insert into States (Abbreviation,Name) values ('FL','Florida');
+insert into States (Abbreviation,Name) values ('GA','Georgia');
+insert into States (Abbreviation,Name) values ('GU','Guam');
+insert into States (Abbreviation,Name) values ('HI','Hawaii');
+insert into States (Abbreviation,Name) values ('ID','Idaho');
+insert into States (Abbreviation,Name) values ('IL','Illinois');
+insert into States (Abbreviation,Name) values ('IN','Indiana');
+insert into States (Abbreviation,Name) values ('IA','Iowa');
+insert into States (Abbreviation,Name) values ('KS','Kansas');
+insert into States (Abbreviation,Name) values ('KY','Kentucky');
+insert into States (Abbreviation,Name) values ('LA','Louisiana');
+insert into States (Abbreviation,Name) values ('ME','Maine');
+insert into States (Abbreviation,Name) values ('MH','Marshall Islands');
+insert into States (Abbreviation,Name) values ('MD','Maryland');
+insert into States (Abbreviation,Name) values ('MA','Massachusetts');
+insert into States (Abbreviation,Name) values ('MI','Michigan');
+insert into States (Abbreviation,Name) values ('MN','Minnesota');
+insert into States (Abbreviation,Name) values ('MS','Mississippi');
+insert into States (Abbreviation,Name) values ('MO','Missouri');
+insert into States (Abbreviation,Name) values ('MT','Montana');
+insert into States (Abbreviation,Name) values ('NE','Nebraska');
+insert into States (Abbreviation,Name) values ('NV','Nevada');
+insert into States (Abbreviation,Name) values ('NH','New Hampshire');
+insert into States (Abbreviation,Name) values ('NJ','New Jersey');
+insert into States (Abbreviation,Name) values ('NM','New Mexico');
+insert into States (Abbreviation,Name) values ('NY','New York');
+insert into States (Abbreviation,Name) values ('NC','North Carolina');
+insert into States (Abbreviation,Name) values ('ND','North Dakota');
+insert into States (Abbreviation,Name) values ('MP','Northern Mariana Islands');
+insert into States (Abbreviation,Name) values ('OH','Ohio');
+insert into States (Abbreviation,Name) values ('OK','Oklahoma');
+insert into States (Abbreviation,Name) values ('OR','Oregon');
+insert into States (Abbreviation,Name) values ('PW','Palau');
+insert into States (Abbreviation,Name) values ('PA','Pennsylvania');
+insert into States (Abbreviation,Name) values ('PR','Puerto Rico');
+insert into States (Abbreviation,Name) values ('RI','Rhode Island');
+insert into States (Abbreviation,Name) values ('SC','South Carolina');
+insert into States (Abbreviation,Name) values ('SD','South Dakota');
+insert into States (Abbreviation,Name) values ('TN','Tennessee');
+insert into States (Abbreviation,Name) values ('TX','Texas');
+insert into States (Abbreviation,Name) values ('UT','Utah');
+insert into States (Abbreviation,Name) values ('VT','Vermont');
+insert into States (Abbreviation,Name) values ('VI','Virgin Islands');
+insert into States (Abbreviation,Name) values ('VA','Virginia');
+insert into States (Abbreviation,Name) values ('WA','Washington');
+insert into States (Abbreviation,Name) values ('WV','West Virginia');
+insert into States (Abbreviation,Name) values ('WI','Wisconsin');
+insert into States (Abbreviation,Name) values ('WY','Wyoming');
+
+insert into Cyber_Security_Questions (Questions_ID, Answers) values ('??','??');
+
+insert into cyber_security_clearence      ;
+
+insert into address      ;
+insert into application      ;
+insert into certifications_financials      ;
+insert into company_information      ;
+insert into company_sector      ;
+insert into company_subsector      ;
+insert into country      ;
+insert into customer      ; 
+insert into foreign_associates      ;
+insert into funding      ;
+insert into imported_materials      ;
+insert into investors      ;
+insert into material      ; 
+insert into naics_codes      ;
+insert into negative_regulatory_actions      ;
+insert into patents      ;
+insert into revenue      ;
+insert into sectors      ;
+insert into subsectors      ;
+
+
+COMMIT;
+
+SELECT insert into  FROM address;
+SELECT insert into  FROM application;
+SELECT insert into  FROM certifications_financials;
+SELECT insert into  FROM company_information;
+SELECT insert into  FROM company_sector;
+SELECT insert into  FROM company_subsector;
+SELECT insert into  FROM country;
+SELECT insert into  FROM customer; 
+SELECT insert into  FROM cyber_security_clearence;
+SELECT insert into  FROM cyber_security_questions;
+SELECT insert into  FROM foreign_associates;
+SELECT insert into  FROM funding;
+SELECT insert into  FROM imported_materials;
+SELECT insert into  FROM investors;
+SELECT insert into  FROM material; 
+SELECT insert into  FROM naics_codes;
+SELECT insert into  FROM negative_regulatory_actions;
+SELECT insert into  FROM patents;
+SELECT insert into  FROM revenue;
+SELECT insert into  FROM sectors;
+SELECT insert into  FROM states;
+SELECT insert into  FROM subsectors;
