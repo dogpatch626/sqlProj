cf70510662bfeff9003e84daefbd39cef767a783
Bin Huang <44174709+ibhuang@users.noreply.github.com>
2020-12-02 17:49:39 -0500

    Update DML.sql
    aa

--- DML.sql
+++ DML.sql
@@ -21,13 +21,13 @@ delete from Sectors;
 delete from States;
 delete from Subsectors;
 
-ALTER Table foreign_associates DROP COLUMN Type_Foreign;
-describe foreign_associates;
-ALTER Table foreign_associates ADD COLUMN Type_Foreign varchar(25);
-ALTER Table foreign_associates ADD COLUMN Type_Foreign22 bigint(11) Default "12121212";
-ALTER Table foreign_associates ADD Constraint Foreign_Associates_FK3 FOREIGN KEY (Type_Foreign22) REFERENCES States(State_ID);
-ALTER Table foreign_associates Drop Constraint Foreign_Associates_FK3;
-ALTER Table foreign_associates DROP COLUMN Type_Foreign22;
+ALTER Table Foreign_associates DROP COLUMN Type_Foreign;
+describe Foreign_associates;
+ALTER Table Foreign_associates ADD COLUMN Type_Foreign varchar(25);
+ALTER Table Foreign_associates ADD COLUMN Type_Foreign22 bigint(11) Default "12121212";
+ALTER Table Foreign_associates ADD Constraint Foreign_Associates_FK3 FOREIGN KEY (Type_Foreign22) REFERENCES States(State_ID);
+ALTER Table Foreign_associates Drop Constraint Foreign_Associates_FK3;
+ALTER Table Foreign_associates DROP COLUMN Type_Foreign22;
 
 insert into States (Abbreviation,Name) values ('AL','Alabama');
 insert into States (Abbreviation,Name) values ('AK','Alaska');
